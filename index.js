4e0e29879b5ac2a135eaa8445a16fcfabb0fc17e
jiyeong <jiyeong0218@naver.com>
2022-11-07 23:08:30 +0900

    -

--- index.js
+++ index.js
@@ -2,37 +2,121 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const config = require('./config/key');
-const { User } = require("./models/User");
+const {auth} = require('./middleware/auth')
+const {User} = require("./models/User");
 
 
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(cookieParser())
 
 
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const {
+    json
+} = require('body-parser');
 
-mongoose.connect(config.mongoURI).then(()=> console.log('MongoDB Connected...'))
-.catch(err => console.log(err))
+
+
+mongoose.connect(config.mongoURI).then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err))
+
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World! 냔냐냐냐')
+    res.send('Hello World! 냔냐냐냐')
 })
 
-app.post('/register',(req, res) => {
-  const user = new User(req.body)
-  user.save((err, userInfo) =>{
-    if(err) return res.json({success : false, err})
-    return res.status(200).json({
-      success: true
+
+
+app.post('/api/users/register', (req, res) => {
+
+    const user = new User(req.body)
+
+
+    user.save((err, userInfo) => {
+        if (err) return res.json({
+            success: false,
+            err
+        })
+        return res.status(200).json({
+            success: true
+        })
     })
-  })
+})
+
+app.post('/api/users/login', (req, res) => {
+
+    // 요청된 이메일이 데이터베이스에 있는지 찾는다.
+    User.findOne({
+        email: req.body.email
+    }, (err, user) => {
+        if (!user) {
+            return res.json({
+                loginSuccess: false,
+                message: "제공된 이메일에 해당하는 유저가 없습니다."
+            })
+        }
+
+
+        // 요청된 이메일이 db에 있다면, 비번이 일치하는지 확인한당
+
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch) return res.json({
+                loginSeccess: false,
+                message: "비번 틀림;;"
+            })
+            // 비번까지 맞다? 그럼 토큰 생성하기
+            user.generateToken((err, user) => {
+                if(err) return res.status(400).send(err);
+
+                // 토큰을 쿠키 || 로컬 스토리지 ... etc 에 저장한다.
+                res.cookie("x_auth", user.token)
+                .status(200)
+                .json({loginSeccess : true , userId: user._id})
+            })
+        })
+    })
+})
+
+
+app.get('/api/users/auth', auth , (req, res) => {
+
+    // 요기까지 미들웨어를 통과했다는 것?
+    // == Authentication이 true라는 것~~~~~~~~~~
+
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name :req.user.name,
+        lastname : req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
+    })
+})
+
+
+
+app.get('/api/users/logout', auth, (req, res) => {
+    User.findOneAndUpdate({_id: req.user._id},
+        { token: "" }
+        , (err, user) => {
+            if(err) return res.json({success:false, err});
+            return res.status(200).send({
+                success: true
+            })
+        })
 })
 
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
-
